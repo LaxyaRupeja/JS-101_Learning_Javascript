0733eb69375c65146a6d9025d30ec2fad1091ba6
Laxya <altlakshi@gmail.com>
2022-12-24 21:40:07 +0530

    Maths_Operator

---
+++ Mathematical_Operator/p4.js
@@ -0,0 +1,15 @@
+let a = 200;
+let b = 3000;
+let c = 58;
+if(a>b && a>c){
+  console.log(a,"is the greatest")
+}
+else if(b> c&& b>a ){
+  console.log(b,"is the greatest")
+}
+else if(c>a&& c>b){
+  console.log(c,"is the greatest")
+}
+else{
+  console.log("All Number are equal")
+}
---
+++ Mathematical_Operator/p5.js
@@ -0,0 +1,18 @@
+let day = "Tue"
+switch(day){
+  case "Mon" : console.log("Monday");
+    break;
+    case "Tue" : console.log("Tuesday");
+    break;
+    case "Wed" : console.log("Wednesday");
+    break;
+    case "Thru" : console.log("Thursday");
+    break;
+    case "Fri" : console.log("Friday");
+    break;
+    case "Sat" : console.log("Saturday");
+    break;
+    case "Sun" : console.log("Sunday");
+    break;
+    default : console.log("Input Error")
+}
